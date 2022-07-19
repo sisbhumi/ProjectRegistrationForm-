26fb674d67ec81b3c0cb40701ed9d0d68c369255
sisbhumi <87226560+sisbhumi@users.noreply.github.com>
2022-07-19 12:19:12 +0530

    Registration From

---
+++ app.js
@@ -0,0 +1,9 @@
+// function form(){
+//     var name = click"input[type ='text']"
+//     var id = getElementById("f_id");
+//     // console.log(name,id);
+//     popup.classList.toggle("show");
+// }
+$("button").click(function(){
+    alert("Name: "+ $("#f_name").val() + "\r\n" + "Mail id:" + $("#f_mail").val());
+  });
